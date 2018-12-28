d30f2145fb5b00fd65d964a588ea5f75b4b884c7
Diogo Campos <d.diogocampos@gmail.com>
2018-12-28 17:02:57 +0000

    Splitted dataset

--- Project2/utils.py
+++ Project2/utils.py
@@ -1,4 +1,4 @@
-import os
+import os, random, shutil
 import xml.etree.ElementTree as ET
 
 classes = {'arrabida': 0, 'camara': 1, 'clerigos': 2, 'musica': 3, 'serralves': 4}
@@ -32,6 +32,38 @@ def read_file(path):
     str = '{} {},{},{},{},{}'.format(imgpath, x_min, y_min, x_max, y_max, obj_id)
 
     print(str)
+
+def split_dataset():
+    for dirname, dirnames, filenames in os.walk('./images'):
+        for class_name in dirnames:
+            class_path = os.path.join(dirname, class_name)
+            class_path, class_dirs, class_files = next(os.walk(class_path))
+            class_count = len(class_files)
+
+            random.shuffle(class_files)
+            split_point = int(0.8 * class_count)
+
+            train_files = class_files[:split_point]
+            val_files = class_files[split_point:]
+
+            train_dir = './data/train/{}'.format(class_name)
+            if not os.path.exists(train_dir):
+                os.makedirs(train_dir)
+                
+            val_dir = './data/validation/{}'.format(class_name)
+            if not os.path.exists(val_dir):
+                os.makedirs(val_dir)
+            
+            for train_image in train_files:
+                src_path = class_path + '/' + train_image
+                dest_path = train_dir + '/' + train_image
+                shutil.copyfile(src_path, dest_path)
+            
+            for val_image in val_files:
+                src_path = class_path + '/' + val_image
+                dest_path = val_dir + '/' + val_image
+                shutil.copyfile(src_path, dest_path)
+
 
 if __name__=='__main__':
     main()
