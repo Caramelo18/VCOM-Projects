f597706a9f543e1392060d90f4d6f0ddbe989050
Diogo Campos <d.diogocampos@gmail.com>
2018-12-31 18:43:59 +0000

    Fix bugs

--- Project2/detection/train.py
+++ Project2/detection/train.py
@@ -21,7 +21,8 @@ def _main():
     if os.environ.get('DEV_ENV'):
         base_path = ''
 
-    annotation_path = base_path + 'train.txt'
+    train_annotations_path = base_path + 'train.txt'
+    validation_annotations_path = base_path + 'validation.txt'
     log_dir = base_path + 'logs/002/'
     classes_path = base_path + 'model_data/vcom_classes.txt'
     anchors_path = base_path + 'model_data/yolo_anchors.txt'
@@ -45,14 +46,13 @@ def _main():
     reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=3, verbose=1)
     early_stopping = EarlyStopping(monitor='val_loss', min_delta=0, patience=10, verbose=1)
 
-    val_split = 0.1
-    with open(annotation_path) as f:
-        lines = f.readlines()
-    np.random.seed(10101)
-    np.random.shuffle(lines)
-    np.random.seed(None)
-    num_val = int(len(lines)*val_split)
-    num_train = len(lines) - num_val
+    with open(train_annotations_path) as f:
+        train_lines = f.readlines()
+    num_train = len(train_lines)
+
+    with open(validation_annotations_path) as f:
+        validation_lines = f.readlines()
+    num_val = len(validation_lines)
 
     # Train with frozen layers first, to get a stable loss.
     # Adjust num epochs to your dataset. This step is enough to obtain a not bad model.
@@ -63,9 +63,9 @@ def _main():
 
         batch_size = 32
         print('Train on {} samples, val on {} samples, with batch size {}.'.format(num_train, num_val, batch_size))
-        model.fit_generator(data_generator_wrapper(lines[:num_train], batch_size, input_shape, anchors, num_classes),
+        model.fit_generator(data_generator_wrapper(train_lines, batch_size, input_shape, anchors, num_classes),
                 steps_per_epoch=max(1, num_train//batch_size),
-                validation_data=data_generator_wrapper(lines[num_train:], batch_size, input_shape, anchors, num_classes),
+                validation_data=data_generator_wrapper(validation_lines, batch_size, input_shape, anchors, num_classes),
                 validation_steps=max(1, num_val//batch_size),
                 epochs=20,
                 initial_epoch=0,
@@ -82,9 +82,9 @@ def _main():
 
         batch_size = 16 # note that more GPU memory is required after unfreezing the body
         print('Train on {} samples, val on {} samples, with batch size {}.'.format(num_train, num_val, batch_size))
-        model.fit_generator(data_generator_wrapper(lines[:num_train], batch_size, input_shape, anchors, num_classes),
+        model.fit_generator(data_generator_wrapper(train_lines, batch_size, input_shape, anchors, num_classes),
             steps_per_epoch=max(1, num_train//batch_size),
-            validation_data=data_generator_wrapper(lines[num_train:], batch_size, input_shape, anchors, num_classes),
+            validation_data=data_generator_wrapper(validation_lines, batch_size, input_shape, anchors, num_classes),
             validation_steps=max(1, num_val//batch_size),
             epochs=35,
             initial_epoch=20,
