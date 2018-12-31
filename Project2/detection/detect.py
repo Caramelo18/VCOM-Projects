8afa5c60bee578f4f0984a41e11dbb1a6bfa5bf1
Diogo Campos <d.diogocampos@gmail.com>
2018-12-31 17:01:05 +0000

    Generate predicted txt file

--- Project2/detection/detect.py
+++ Project2/detection/detect.py
@@ -2,19 +2,33 @@ import os
 from yolo import YOLO
 from PIL import Image
 
+classes = {'arrabida': 0, 'camara': 1, 'clerigos': 2, 'musica': 3, 'serralves': 4}
+
 def main():
     print(os.environ.get('DEV_ENV'))
+    open('predicted.txt', 'w').close()
+
     yolo = YOLO()
+    
+    for dirname, dirnames, filenames in os.walk('../data/validation'):
+        for class_name in dirnames:
+            class_path = os.path.join(dirname, class_name)
+            class_path, class_dirs, class_files = next(os.walk(class_path))
 
-    #image = Image.open('../data/images/arrabida/arrabida-0043.jpg')
-    #image = Image.open('../data/images/camara/camara-0043.jpg')
-    #image = Image.open('../data/images/clerigos/clerigos-0043.jpg')
-    image = Image.open('../data/images/musica/musica-0145.jpg')
-    #image = Image.open('../data/images/serralves/serralves-0107.jpg')
-    #image = Image.open('/home/fabio/Documentos/5o Ano/VCOM/ex2.png')
+            for image_name in class_files:
+                image_path = os.path.join(class_path, image_name)
+                image = Image.open(image_path)
+    
+                boxes = yolo.detect_image_return_boxes(image)
 
-    r_image = yolo.detect_image(image)
-    r_image.show()
+                line = image_path
+                for box in boxes:
+                    (left, top, right, bottom, predicted_class, score) = box
+                    box_str = '{},{},{},{},{},{}'.format(left, top, right, bottom, classes[predicted_class], score)
+                    line += ' ' + box_str
+                line += '\n'
+                with open('predicted.txt', 'a', encoding='utf-8') as file:
+                    file.write(line)
 
     yolo.close_session()
 
