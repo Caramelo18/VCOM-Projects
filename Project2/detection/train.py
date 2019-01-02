550487401c93f823aab4120eea26bd6f73ef36d2
Caramelo18 <fabiocaramelo18@gmail.com>
2019-01-02 00:27:04 +0000

    added training log for yolo and prepared training for tiny_yolo

--- Project2/detection/train.py
+++ Project2/detection/train.py
@@ -23,9 +23,9 @@ def _main():
 
     train_annotations_path = base_path + 'train.txt'
     validation_annotations_path = base_path + 'validation.txt'
-    log_dir = base_path + 'logs/002/'
+    log_dir = base_path + 'logs/003/'
     classes_path = base_path + 'model_data/vcom_classes.txt'
-    anchors_path = base_path + 'model_data/yolo_anchors.txt'
+    anchors_path = base_path + 'model_data/tiny_yolo_anchors.txt'
     class_names = get_classes(classes_path)
     num_classes = len(class_names)
     anchors = get_anchors(anchors_path)
@@ -80,7 +80,7 @@ def _main():
         model.compile(optimizer=Adam(lr=1e-4), loss={'yolo_loss': lambda y_true, y_pred: y_pred}) # recompile to apply the change
         print('Unfreeze all of the layers.')
 
-        batch_size = 16 # note that more GPU memory is required after unfreezing the body
+        batch_size = 32 # note that more GPU memory is required after unfreezing the body
         print('Train on {} samples, val on {} samples, with batch size {}.'.format(num_train, num_val, batch_size))
         model.fit_generator(data_generator_wrapper(train_lines, batch_size, input_shape, anchors, num_classes),
             steps_per_epoch=max(1, num_train//batch_size),
