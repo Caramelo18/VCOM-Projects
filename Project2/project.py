1d4be67a54b43b02c2f768bc5225232c88635c99
Diogo Campos <d.diogocampos@gmail.com>
2019-01-07 18:45:50 +0000

    add stats

--- Project2/project.py
+++ Project2/project.py
@@ -21,6 +21,7 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 import pandas as pd
+import os
 
 seed(1143)
 set_random_seed(1143)
@@ -57,10 +58,57 @@ def main():
     parser = ArgumentParser()
     parser.add_argument("-c", "--classify", type=classify, action="store", help="classify an image on the test folder")
     parser.add_argument("-tc", "--train_classification", action="store_true")
+    parser.add_argument("-s", "--stats", action='store_true')
+    parser.add_argument("-e", "--eval", action='store_true')
 
     args = parser.parse_args()
     if args.train_classification:
         train_classification()
+    elif args.eval:
+        eval()
+
+
+def stats():
+    model = load_model('my_model.h5')
+
+    classes = ['arrabida', 'camara', 'clerigos', 'musica', 'serralves']
+
+    results = dict.fromkeys(classes, {})
+
+    for dirname, dirnames, filenames in os.walk('./data/validation'):
+        for class_name in dirnames:
+            results[class_name] = dict.fromkeys(classes, 0)
+
+            class_path = os.path.join(dirname, class_name)
+            class_path, class_dirs, class_files = next(os.walk(class_path))
+
+            for image_name in class_files:
+                image_path = os.path.join(class_path, image_name)
+                img = image.load_img(image_path, target_size=(image_size, image_size))
+                img = image.img_to_array(img)
+                img = np.expand_dims(img, axis=0)
+                img = preprocess_input(img)
+
+                pred = model.predict(img)
+
+                predicted_class_indices = np.argmax(pred,axis=1)
+
+                labels = (train_generator.class_indices)
+                labels = dict((v,k) for k,v in labels.items())
+                predictions = [labels[k] for k in predicted_class_indices]
+
+                predicted_class = predictions[0]
+                results[class_name][predicted_class] += 1
+    
+    print(results)
+
+
+def eval():
+    model = load_model('my_model.h5')
+
+    scoreSeg = model.evaluate_generator(validation_generator, 418)
+    print("Accuracy = ",scoreSeg[1])
+
 
 def classify(file):
     model = load_model('128d-1-100-5-120.h5')
